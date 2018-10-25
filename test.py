2693234cd02b1c718cef181ff3f663351e8824b4
Nikola Valesova <nikca.valesova@gmail.com>
2018-10-25 09:49:26 +0200

    Initial commit

---
+++ test.py
@@ -0,0 +1,32 @@
+import os
+
+examples = os.listdir(os.curdir + '/Examples')
+
+# if Makefile is present, make the source files
+if 'Makefile' in os.listdir(os.curdir):
+    os.system('make clean')
+    os.system('make')
+
+# if Makefile is missing and executables are not found, exit
+elif 'bms1A' not in os.listdir(os.curdir) or 'bms1B' not in os.listdir(os.curdir):
+    print('Executable files \'bms1A\' and/or \'bms1B\' not found!')
+    exit()
+
+txt_examples = sorted([x for x in examples if x[-3:] == 'txt'])
+wav_examples = sorted([x for x in examples if x[-3:] == 'wav'])
+
+# execute modulation and demodulation
+for example in txt_examples:
+    os.system('./bms1A Examples/' + example)
+    os.system('./bms1B Examples/' + example[:-3] + 'wav')
+
+for example in wav_examples:
+    os.system('./bms1B Examples/' + example)
+
+outputs = os.listdir(os.curdir + '/Examples')
+txt_outputs = sorted([x for x in outputs if x[-3:] == 'txt'])
+
+# compare txt files
+print('\n\nComparing output files...')
+for example in txt_outputs:
+    os.system('diff -s ref.txt Examples/' + example)
